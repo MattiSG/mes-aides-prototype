7e64d8d1512d978da07ff49d7c14974c236e43e6
Matti Schneider <hi@mattischneider.fr>
2014-03-14 16:20:24 +0100

    Simulate on OpenFisca

---
+++ request.js
@@ -0,0 +1,61 @@
+var statmatMap = {
+    'seul': 'Célibataire',
+    'en concubinage': 'Célibataire',
+    'pacsé.e': 'Pacsé',
+    'marié.e': 'Marié'
+};
+
+var tenancyMap = {
+    'locataire': 'Locataire ou sous-locataire d\'un logement loué vide non-HLM',
+    'propriétaire': 'Propriétaire (non accédant) du logement'
+};
+
+function simulate(v) {
+    var dfd = $.Deferred();
+
+    var firstPerson = {
+        activite: 'Actif occupé',
+        birth: (2014 - parseInt(v.age, 10)) + '-01-01',
+        id: 'ind0',
+        sali: (parseInt(v.minus1) + parseInt(v.minus2) + parseInt(v.minus3)) * 4,
+        statmarit: statmatMap[v.marital]
+    };
+    var secondPerson = _.clone(firstPerson);
+    secondPerson.id = 'ind1';
+    var individus = v.marital === 'seul' ? [firstPerson] : [firstPerson, secondPerson];
+    var menages = {
+        personne_de_reference: 'ind0',
+        so: tenancyMap[v.tenancy],
+        loyer: parseInt(v.rent)
+    };
+    var scenario = {
+        test_case: {
+            familles: [
+                { parents: ['ind0'] }
+            ],
+            foyers_fiscaux: [
+                { declarants: ['ind0'] }
+            ],
+            individus: individus,
+            menages: [menages]
+        },
+        legislation_url: 'http://api.openfisca.fr/api/1/default-legislation',
+        year: 2013
+    };
+
+    $.ajax({
+        method: 'POST',
+        url: 'http://api.openfisca.fr/api/1/simulate',
+        data: JSON.stringify({ scenarios: [scenario] }),
+        dataType: 'json',
+        contentType: 'application/json'
+    }).done(function(data) {
+        var result = {};
+        result[data.value.children[2].children[1].children[0].name] = data.value.children[2].children[1].children[0].values[0];
+        result['Aides au logement'] = data.value.children[2].children[2].values[0];
+        result[data.value.children[2].children[1].children[4].name] = data.value.children[2].children[1].children[4].values[0];
+        dfd.resolve(result);
+    });
+
+    return dfd.promise();
+}
